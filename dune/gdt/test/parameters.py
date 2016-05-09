8d340dc049c1f2edb905ac171ea6356772b6a8cb
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-05-09 11:21:13 +0200

    [test.parameters] fix indexing and ensure that sigma_t = sigma_s + sigma_a

--- dune/gdt/test/parameters.py
+++ dune/gdt/test/parameters.py
@@ -1,26 +1,25 @@
 from boltzmann import wrapper
 from mpi4py import MPI
-import numpy as np
 
 comm_world = MPI.COMM_WORLD
 size_world = comm_world.Get_size()
 rank_world = comm_world.Get_rank()
 
-sigma_s_scattering_range = np.arange(0, 1, 0.3)
-sigma_s_absorbing_range = np.arange(0, 1, 0.3)
-sigma_t_scattering_range = range(0, 11, 3)
-sigma_t_absorbing_range = range(0, 11, 3)
+sigma_s_scattering_range = range(0, 11, 3)
+sigma_s_absorbing_range = range(0, 8, 3)
+sigma_a_scattering_range = range(0, 11, 3)
+sigma_a_absorbing_range = range(0, 11, 3)
 
 parameters_list=[]
 for sigma_s_scattering in sigma_s_scattering_range:
     for sigma_s_absorbing in sigma_s_absorbing_range:
-        for sigma_t_scattering in sigma_t_scattering_range:
-            for sigma_t_absorbing in sigma_s_absorbing_range:
-                parameters_list.append([sigma_s_scattering, sigma_s_absorbing, sigma_t_scattering, sigma_t_absorbing])
+        for sigma_a_scattering in sigma_a_scattering_range:
+            for sigma_a_absorbing in sigma_a_absorbing_range:
+                parameters_list.append([sigma_s_scattering, sigma_s_absorbing, sigma_s_scattering+sigma_a_scattering, sigma_s_absorbing+sigma_a_absorbing])
 
 parameters=comm_world.scatter(parameters_list, root=0)
 
-solver=wrapper.Solver(1, "boltzmann_sigma_s_s_" + str(parameters[1]) + "_a_" + str(parameters[2]) + "sigma_t_s_" + str(parameters[3]) + "_a_" + str(parameters[4]), 2, 50, True, True, parameters[1], parameters[2], parameters[3], parameters[4])
+solver=wrapper.Solver(1, "/scratch/tmp/l_tobi01/boltzmann_sigma_s_s_" + str(parameters[0]) + "_a_" + str(parameters[1]) + "sigma_t_s_" + str(parameters[2]) + "_a_" + str(parameters[3]), 1, 100, True, True, parameters[0], parameters[1], parameters[2], parameters[3])
 solver.solve()
 print(parameters, " done ")
 
