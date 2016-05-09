df4f13ad8b4df6eea55c867edf062235469448f8
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-05-09 10:42:46 +0200

    [test.boltzmann_mpi] use HAPOD

--- dune/gdt/test/boltzmann_mpi.py
+++ dune/gdt/test/boltzmann_mpi.py
@@ -1,58 +1,71 @@
 from boltzmann import wrapper
 from pymor.basic import *
 from mpi4py import MPI
+import numpy as np
 
 comm_world = MPI.COMM_WORLD
 size_world = comm_world.Get_size()
 rank_world = comm_world.Get_rank()
 
 # gather processor names and assign each processor name a unique positive number
-processor_name = MPI.Get_processor_name()
-processor_names = comm_world.gather(processor_name, root=0)
-processor_numbers = dict()
-if rank_world == 0:
-    processor_set = set(processor_names)
-    processor_numbers = dict.fromkeys(processor_set, 0)
-    current_num = 0
-    for processor_key in processor_numbers:
-        processor_numbers[processor_key] = current_num
-        current_num += 1
-processor_numbers = comm_world.bcast(processor_numbers, root=0)
+proc_name = MPI.Get_processor_name()
+proc_names = comm_world.allgather(proc_name)
+proc_numbers = dict.fromkeys(set(proc_names), 0)
+for i, proc_key in enumerate(proc_numbers):
+    proc_numbers[proc_key] = i
 
 # use processor numbers to create a communicator on each processor
-comm_proc = MPI.Intracomm.Split(comm_world, processor_numbers[processor_name], rank_world)
+comm_proc = MPI.Intracomm.Split(comm_world, proc_numbers[proc_name], rank_world)
 size_proc = comm_proc.Get_size()
 rank_proc = comm_proc.Get_rank()
 
 # create communicator containing rank 0 processes on each processor
-contained_in_rank_0_group = 0
-if rank_proc == 0:
-    contained_in_rank_0_group = 1
+contained_in_rank_0_group = 1 if rank_proc == 0 else 0
 comm_rank_0_group = MPI.Intracomm.Split(comm_world, contained_in_rank_0_group, rank_world)
 
 # calculate Boltzmann problem trajectory (using one thread per process)
-solver = wrapper.Solver(1, "boltzmann", 2000000, 200, False, True)
+solver = wrapper.Solver(1, "boltzmann", 2000000, 50, False, False)
 result = solver.solve()
+num_snapshots=len(result)
 
 # get pod modes from each trajectory
-pod_modes, ignored_singular_values = pod(result)
+epsilon_ast = 4e-4
+omega=0.5
+rooted_tree_depth=3
+modes, singular_values = pod(result, atol=0., rtol=0., l2_mean_err=epsilon_ast*omega/np.sqrt(rooted_tree_depth-1))
+modes.scal(singular_values)
 
-# gather pod modes from trajectories on processor, join to one VectorArray and perform a second pod per processor
-pod_modes_on_processor = comm_proc.gather(pod_modes, root=0)
-pod_modes_on_processor_joined = pod_modes.empty()
-second_pod_modes = 0
+# gather scaled pod modes from trajectories on processor, join to one VectorArray and perform a second pod per processor
+modes_on_proc = comm_proc.gather(modes, root=0)
+num_snapshots_on_proc = comm_proc.reduce(num_snapshots, op=MPI.SUM, root=0)
 if rank_proc == 0:
-    for vectorarray in pod_modes_on_processor:
-        pod_modes_on_processor_joined.append(vectorarray)
-    second_pod_modes, ignored_singular_values = pod(pod_modes_on_processor_joined)
+    modes_on_proc_joined = modes.empty()
+    for vectorarray in modes_on_proc:
+        modes_on_proc_joined.append(vectorarray)
+    epsilon_T_alpha = epsilon_ast*omega*np.sqrt(num_snapshots_on_proc/(len(modes_on_proc_joined)*(rooted_tree_depth-1)))
+    print("epsilon_t_alpha: ", epsilon_T_alpha)
+    second_modes, second_singular_values = pod(modes_on_proc_joined, atol=0., rtol=0., l2_mean_err=epsilon_T_alpha)
+    second_modes.scal(second_singular_values)
 
-# gather all pod modes from second pod on world rank 0 and perform a third pod with the joined pod modes
-all_second_pod_modes = comm_rank_0_group.gather(second_pod_modes, root=0)
-all_second_pod_modes_joined = pod_modes.empty()
-final_pod_modes = 0
-final_singular_values = 0
+# gather all scaled pod modes from second pod on world rank 0 and perform a third pod with the joined pod modes
+if rank_proc == 0:
+    all_second_modes = comm_rank_0_group.gather(second_modes, root=0)
+    total_num_snapshots = comm_rank_0_group.reduce(num_snapshots_on_proc, op=MPI.SUM, root=0)
+
+final_modes=0
 if rank_world == 0:
-    for vectorarray in all_second_pod_modes:
-        all_second_pod_modes_joined.append(vectorarray)
-    final_pod_modes, final_singular_values = pod(all_second_pod_modes_joined)
-    print(final_pod_modes, final_singular_values)
+    all_second_modes_joined = modes.empty()
+    for vectorarray in all_second_modes:
+        all_second_modes_joined.append(vectorarray)
+    epsilon_T_gamma = epsilon_ast*(1-omega)*np.sqrt(total_num_snapshots/len(all_second_modes_joined))
+    print("epsilon_t_gamma: ", epsilon_T_gamma)
+    final_modes, final_singular_values = pod(all_second_modes_joined, atol=0., rtol=0., l2_mean_err=epsilon_T_gamma)
+    print(final_singular_values)
+    #final_modes.scal(final_singular_values)
+final_modes=comm_world.bcast(final_modes, root=0)
+
+trajectory_error = np.sum((result - final_modes.lincomb(result.dot(final_modes))).l2_norm()**2)
+trajectory_errors = comm_world.gather(trajectory_error, root=0)
+if rank_world == 0:
+    error=np.sqrt(np.sum(trajectory_errors)/total_num_snapshots)
+    print(error)
