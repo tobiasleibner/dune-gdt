8b1cf31af9e51c5204e0f365cdb1da7be4ec0869
Tobias Leibner <tobias.leibner@uni-muenster.de>
2016-05-10 10:39:19 +0200

    [test.boltzmann_mpi] use Gather instead of gather

--- dune/gdt/test/boltzmann_mpi.py
+++ dune/gdt/test/boltzmann_mpi.py
@@ -1,7 +1,9 @@
 from boltzmann import wrapper
 from pymor.basic import *
+from pymor.vectorarrays.list import NumpyVector
 from mpi4py import MPI
 import numpy as np
+import resource
 
 comm_world = MPI.COMM_WORLD
 size_world = comm_world.Get_size()
@@ -39,42 +41,46 @@ for sigma_s_scattering in sigma_s_scattering_range:
 parameters=comm_world.scatter(parameters_list, root=0)
 
 # calculate Boltzmann problem trajectory (using one thread per process)
-solver=wrapper.Solver(1, "/scratch/tmp/l_tobi01/boltzmann_sigma_s_s_" + str(parameters[0]) + "_a_" + str(parameters[1]) + "sigma_t_s_" + str(parameters[2]) + "_a_" + str(parameters[3]), 2000000, 100, False, False, parameters[0], parameters[1], parameters[2], parameters[3])
+solver=wrapper.Solver(1, "boltzmann_sigma_s_s_" + str(parameters[0]) + "_a_" + str(parameters[1]) + "sigma_t_s_" + str(parameters[2]) + "_a_" + str(parameters[3]), 2000000, 20, False, False, parameters[0], parameters[1], parameters[2], parameters[3])
 result = solver.solve()
 num_snapshots=len(result)
+vector_length=result.dim
 
 # get pod modes from each trajectory
-epsilon_ast = 4e-8
+epsilon_ast = 1e-8
 omega=0.5
 rooted_tree_depth=3
 modes, singular_values = pod(result, atol=0., rtol=0., l2_mean_err=epsilon_ast*omega/np.sqrt(rooted_tree_depth-1))
 modes.scal(singular_values)
 
 # gather scaled pod modes from trajectories on processor, join to one VectorArray and perform a second pod per processor
-modes_on_proc = comm_proc.gather(modes, root=0)
 num_snapshots_on_proc = comm_proc.reduce(num_snapshots, op=MPI.SUM, root=0)
+modes_on_proc = np.empty(shape=(num_snapshots_on_proc,vector_length)) if rank_proc == 0 else np.empty(shape=(0,0))
+comm_proc.Gather(modes.data, modes_on_proc, root=0)
 if rank_proc == 0:
-    modes_on_proc_joined = modes.empty()
-    for vectorarray in modes_on_proc:
-        modes_on_proc_joined.append(vectorarray)
+    modes_on_proc_joined = modes.zeros(len(modes_on_proc))
+    for v, vv in zip(modes_on_proc_joined._list, modes_on_proc):
+        v.data[:] = vv
     epsilon_T_alpha = epsilon_ast*omega*np.sqrt(num_snapshots_on_proc/(len(modes_on_proc_joined)*(rooted_tree_depth-1)))
-    print("epsilon_t_alpha: ", epsilon_T_alpha)
     second_modes, second_singular_values = pod(modes_on_proc_joined, atol=0., rtol=0., l2_mean_err=epsilon_T_alpha)
     second_modes.scal(second_singular_values)
+    print("memory used 1: ", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0)
 
 # gather all scaled pod modes from second pod on world rank 0 and perform a third pod with the joined pod modes
+total_num_snapshots = comm_rank_0_group.reduce(num_snapshots_on_proc, op=MPI.SUM, root=0) if rank_world == 0 else 0
+all_second_modes = np.empty(shape=(total_num_snapshots,vector_length)) if rank_world == 0 else np.empty(shape=(0,0))
 if rank_proc == 0:
-    all_second_modes = comm_rank_0_group.gather(second_modes, root=0)
-    total_num_snapshots = comm_rank_0_group.reduce(num_snapshots_on_proc, op=MPI.SUM, root=0)
+    comm_rank_0_group.Gather(second_modes.data, all_second_modes, root=0)
+    print("memory used 2: ", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0)
 
 final_modes=0
 if rank_world == 0:
-    all_second_modes_joined = modes.empty()
-    for vectorarray in all_second_modes:
-        all_second_modes_joined.append(vectorarray)
+    all_second_modes_joined = modes.zeros(len(all_second_modes))
+    for v, vv in zip(all_second_modes_joined._list, all_second_modes):
+        v.data[:] = vv
     epsilon_T_gamma = epsilon_ast*(1-omega)*np.sqrt(total_num_snapshots/len(all_second_modes_joined))
-    print("epsilon_t_gamma: ", epsilon_T_gamma)
     final_modes, final_singular_values = pod(all_second_modes_joined, atol=0., rtol=0., l2_mean_err=epsilon_T_gamma)
+    print("memory used 3: ", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0)
     print(final_singular_values)
     #final_modes.scal(final_singular_values)
 final_modes=comm_world.bcast(final_modes, root=0)
